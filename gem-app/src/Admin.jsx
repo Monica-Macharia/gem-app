b1b3e0b96a5a4bcc38db297a52e02fecba58e132
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-05-14 21:47:58 +0300

    admin html structure

--- gem-app/src/Admin.jsx
+++ gem-app/src/Admin.jsx
@@ -1,7 +1,41 @@
 function Admin(){
+    //create a fetch request that displays all gems and display as cards on a grid
+    // remember to change state
+
     return(
         <>
-        {/* //form for POST */}
+        {/* display fetch results */}
+        <div className="cards">
+            <img src="" alt="" />
+            <p>Name:</p>
+            <p>Video-url:</p>
+            <p>Decription:</p> 
+            
+        {/*PUT form */}
+        <button>EDIT</button>
+
+        {/* DELETE form */}
+        <button>DELETE</button>
+        </div>
+
+       
+
+        {/*form for POST */}
+        <form>
+                   <label>Name:
+                <input type="text" />
+            </label>
+            <label>Image url:
+                <input type="text" />
+            </label>
+            <label>Video url:
+                <input type="text" />
+            </label>
+            <label>Description:
+                <input type="text" />
+            </label>
+
+        </form>
 
         </>
     )
