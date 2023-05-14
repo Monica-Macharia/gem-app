d7b3147d227fdd8593107580b2af6f62d29626ec
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-05-14 20:54:20 +0300

    html structure

--- gem-app/src/App.jsx
+++ gem-app/src/App.jsx
@@ -5,7 +5,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="container">
+
+        <img src="" alt="background-image" />
+        {/* <ul className="navbar">
+          <li>User</li>
+          <li>Admin</li>
+        </ul> */}
+
+
       
       <h1>Vite + React</h1>
       <div className="card">
@@ -19,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
