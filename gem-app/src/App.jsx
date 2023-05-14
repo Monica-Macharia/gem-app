8b05f85d06f1e0b9cabef07a06e03a01c3721bb7
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-05-14 21:00:18 +0300

    cards

--- gem-app/src/App.jsx
+++ gem-app/src/App.jsx
@@ -7,11 +7,20 @@ function App() {
   return (
     <div className="container">
 
-        <img src="" alt="background-image" />
+        <img className='background-image' src="" alt="background-image" />
         {/* <ul className="navbar">
           <li>User</li>
           <li>Admin</li>
         </ul> */}
+        <div className="description">
+          <p>You Know of Another <br/> Hidden Gem?</p>
+          <button className="call-action">Share With Us!</button>
+        </div>
+        <div className="cards">
+          <img src="" alt="card-img"/>
+          <h3>Name</h3>
+        </div>
+        
 
 
       
